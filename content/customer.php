e89cbe923b2934916a1c4244c0a57b4069a84862
ryanrizki10 <ryanrizky.1012@gmail.com>
2025-03-18 11:39:48 +0700

    First Upload

---
+++ content/customer.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card">
+            <div class="card-header">
+                <h3>Data Pelanggan</h3>
+            </div>
+            <div class="card-body"></div>
+        </div>
+    </div>
+</div>
